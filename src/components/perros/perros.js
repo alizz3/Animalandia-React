fdaf965c9785a2a7871a4cb8b2e1d710993194ed
Alizz M <alizz.mejia03@gmail.com>
2022-06-08 21:26:08 -0500

    Animalandia project in react

---
+++ src/components/perros/perros.js
@@ -0,0 +1,72 @@
+let perros = [
+    {
+        "id": 1,
+        "raza": "Schnauzer",
+        "thumbnail": "https://cdn.royalcanin-weshare-online.io/MSIyx2oBaxEApS7LdRyF/v3/bd109h-hub-schnauzer-adult-black-and-white?w=640&fm=jpg&auto=compress",
+        "imagen": "https://www.purina.es/sites/default/files/styles/ttt_image_original/public/2021-02/BREED%20Hero%20Desktop_0033_schnauzer_standard.webp?itok=3oe6Y5QZ",
+        "descripcion": "El Schnauzer estándar es un perro robusto y musculoso de tamaño mediano que tiene unas cejas y bigotes prominentes.",
+        "acercade": "El Schnauzer estándar es un perro robusto y musculoso de tamaño mediano que tiene unas cejas y bigotes prominentes. El pelaje puede ser negro o gris. Los machos adultos miden idealmente unos 48 cm de altura y pesan 18 kilos. Las hembras adultas suelen medir 45,7 cm de media y pesan 16 kg.",
+        "detalle": "Animado / Amoroso / Juguetón / Leal / Alerta / Silencioso / Inteligente / Seguro / Resistente / Trabajador",
+        "tamaño": "Mediano",
+        "vida": "13-16 años"
+    },
+    {
+        "id": 2,
+        "raza": "American Staffordshire terrier",
+        "thumbnail": "https://cdn.royalcanin-weshare-online.io/8iLJxWoBaxEApS7L-RuR/v3/bd126bl-hub-american-staffordshire-terrier-adult-standing?w=320&fm=jpg&auto=compress",
+        "imagen": "https://t2.ea.ltmcdn.com/es/razas/9/6/0/american-staffordshire-terrier_69_0_600.jpg",
+        "descripcion": "Los American Staffordshire terriers son excelentes guardianes, debido a su inteligencia y lealtad.",
+        "acercade": "El American Staffordshire terrier, fuerte, atlético y valiente, es ideal para entrenamientos de obediencia y agilidad. El American Staffordshire terrier es una raza muy afectuosa y sociable con las personas. Ama pasar el tiempo con su compañero humano.Esta raza es amante de la diversión y ha pasado de ser un antiguo ancestro del pit bull terrier a ser un perro familiar mucho más dócil que se lleva bien con otras mascotas, niños y adultos.",
+        "detalle": "Ágil / Entusiasta / Animado",
+        "tamaño": "Mediano",
+        "vida": "12-14 años"
+    },
+    {
+        "id": 3,
+        "raza": "Yorkshire terrier",
+        "thumbnail": "https://cdn.royalcanin-weshare-online.io/DSIIHWsBaxEApS7Lqx3G/v1/bd9bl-hub-yorkshire-terrier-adult-standing?w=320&fm=jpg&auto=compress",
+        "imagen": "https://img.freepik.com/foto-gratis/yorkshire-terrier-cachorro-5-meses-edad-retrato-perro-aislado_191971-3167.jpg?w=2000",
+        "descripcion": "Resueltos y llenos de carácter, los yorkshire terrier son excelentes perros de compañía.",
+        "acercade": "El yorkshire terrier tiene un magnífico pelaje que requiere de aseo regular. A pesar de su apariencia de perro miniatura, sigue siendo un terrier de corazón y conserva su instinto de caza, ya sea por un juguete en la casa o un roedor en el jardín",
+        "detalle": "Alerta / Inteligente / Animado / Apacible / Amistoso",
+        "tamaño": "Muy pequeño",
+        "vida": "15-16 años"
+    },
+    {
+        "id": 4,
+        "raza": "American wirehair",
+        "thumbnail": "https://cdn.royalcanin-weshare-online.io/mmlmHGsBG95Xk-RBsfh-/v1/bc16h-hub-american-wirehair-adult-black-and-white?w=640&fm=jpg&auto=compress",
+        "imagen": "https://i.ytimg.com/vi/9MWQukUJEc8/mqdefault.jpg",
+        "descripcion": "Los american wirehair se han ganado la fama de mascotas familiares populares gracias a su naturaleza relajada y feliz",
+        "acercade": "Los american wirehair son conocidos por ser muy pacientes con los niños. Son tranquilos, pero también pueden ser juguetones, incluso cuando envejecen. En general, son gatos inteligentes y se interesan en todo lo que los rodea.",
+        "detalle": "Tranquilo / Sociable / Silencioso / Afectuoso / Juguetón / Inteligente / Independiente",
+        "tamaño": "Mediano",
+        "vida": "15–16 años"
+    }
+    ,
+    {
+        "id": 5,
+        "raza": "Americano de pelo corto",
+        "thumbnail": "https://i.ytimg.com/vi/_TcJPRZo5po/maxresdefault.jpg",
+        "imagen": "https://cdn.royalcanin-weshare-online.io/lGno1WoBG95Xk-RBn_eJ/v9/bc1h-hub-american-shorthair-adult-black-and-white?w=640&fm=jpg&auto=compress",
+        "descripcion": "Los americanos de pelo corto son gatos relajados y tranquilos, aunque aún exhiben una explosión de energía lúdica, incluso de mayores.",
+        "acercade": "El gato americano de pelo corto, muy juguetón con pelaje corto brillante y que se expresa de forma dulce y clara, refleja el aspecto felino de sus antepasados cazadores. Los americanos de pelo corto son gatos de buen carácter y fáciles de llevar. Son muy populares entre las familias porque tienen una naturaleza lúdica y son pacientes con los niños.",
+        "detalle": "Tranquilo / Sociable / Silencioso / Inteligente / Independiente / Juguetón",
+        "tamaño": "Mediano",
+        "vida": "15-20 años"
+    },
+    {
+        "id": 6,
+        "raza": "Don Sphynx",
+        "thumbnail": "https://cdn.royalcanin-weshare-online.io/dPpyHGsBIYfdNSoCyRpw/v1/bc28h-hub-don-sphynx-adult-black-and-white?w=640&fm=jpg&auto=compress",
+        "imagen": "https://img.freepik.com/foto-gratis/gato-pelo-sphynx-posando-aislado_191971-20552.jpg",
+        "descripcion": "También conocido como Donskoy, el Don Sphynx es un gato intrigante, elegante, único y curioso.",
+        "acercade": "El Don Sphynx es activo, extremadamente amistoso, muy inteligente y amoroso, leal y dedicado a sus dueños. Los Don Sphynx son gatos de buen carácter, dóciles, fáciles de cuidar y manejar. Tienen una personalidad equilibrada, muestran un gran interés por su entorno y disfrutan inventando y jugando.",
+        "detalle": "Activo / Sociable / Afectuoso / Inteligente / Leal",
+        "tamaño": "Mediano",
+        "vida": " 8 años"
+    }
+]
+export function getAllPerros() {
+    return perros;
+}
